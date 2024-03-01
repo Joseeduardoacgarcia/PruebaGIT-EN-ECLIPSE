9b3509843b0e2fe4fbdfa4754a7e4aac20126875
Joseeduardoacgarcia <Joseeduardoacgarcia@gmail.com>
2024-03-01 15:01:54 -0600

    PRIMEROS PASOS

---
+++ src/GIT_NUEVO_FRAME.java
@@ -0,0 +1,48 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+public class GIT_NUEVO_FRAME extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GIT_NUEVO_FRAME frame = new GIT_NUEVO_FRAME();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GIT_NUEVO_FRAME() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(186, 45, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+	}
+
+}
